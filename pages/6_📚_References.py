315be237da93a9489d03ef8c1c5ffe8a1796279a
Andreas Tyrosvoutis <andreas.tyrosvoutis@gmail.com>
2023-09-07 00:59:50 +0300

    s

--- pages/6_📚_References.py
+++ pages/6_📚_References.py
@@ -5,3 +5,7 @@ import numpy as np
 
 st.set_page_config(page_title="References for The Past, The Present & The Future of Bitcoin.", page_icon="📚",layout="wide")
 st.write(st.session_state["shared"])
+
+
+https://dune.com/niftytable/bitcoin-security-budget
+

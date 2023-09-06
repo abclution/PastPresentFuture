dede94c75cfdb8a3d667527ad65ccaa17203f222
Andreas Tyrosvoutis <andreas.tyrosvoutis@gmail.com>
2023-09-07 01:32:32 +0300

    s

--- pages/1_🎇_Genesis.py
+++ pages/1_🎇_Genesis.py
@@ -1,2 +1,7 @@
-past-present-future-of-bitcoin
-🏠
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.set_page_config(page_title="Genesis - The Past, The Present & The Future of Bitcoin.", page_icon="🚸",layout="wide")
+st.write(st.session_state["shared"])

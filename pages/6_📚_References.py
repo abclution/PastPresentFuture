e0856db133d6335d2efd4662bd5eeb562da34501
Andreas Tyrosvoutis <andreas.tyrosvoutis@gmail.com>
2023-09-07 03:36:01 +0300

    s

--- pages/6_📚_References.py
+++ pages/6_📚_References.py
@@ -7,30 +7,33 @@ st.set_page_config(page_title="References for The Past, The Present & The Future
 # st.write(st.session_state["shared"])
 
 
+
 # https://dune.com/niftytable/bitcoin-security-budget
 
-"""
-## Security Budget
+with st.expander("**Security Budget**", expanded=False):
+    """
+    ## Security Budget
 
-The term **"Security Budget"** typically refers to the resources and mechanisms allocated and spent to secure the Bitcoin network.
+    The term **"Security Budget"** typically refers to the resources and mechanisms allocated and spent to secure the Bitcoin network.
 
-It can be assessed in the simple economic terms of :green[Income] vs :red[Expenditures].
+    It can be assessed in the simple economic terms of :green[Income] vs :red[Expenditures].
 
-### :green[Income Items]
-- 💰 Block Reward, a reward of newly mined Bitcoin given to the winning miner, per block.
-- 🤑 Transaction Fees, users of Bitcoin pay a fee to send transactions.
-
-
-### :red[Expenditures Items]
-- 🔌 Electricity Cost
-    - the cost of running Bitcoin mining hardware.
-- 🖧 Hardware Purchases & Failures
-    - ex. Bitcoin Mining Equipment, Air Conditioners, Wiring etc.
-- 💸 Rent, taxes, licenses, salaries and other wages for the running of the Bitcoin mining operation.
-
-In this presentation from the Expenditures items, we will only focus on the Electricity Cost, as it is the majority costs of any Bitcoin Mining operation.
+    ### :green[Income Items]
+    - 💰 Block Reward, a reward of newly mined Bitcoin given to the winning miner, per block.
+    - 🤑 Transaction Fees, users of Bitcoin pay a fee to send transactions.
 
 
-### :red[Simply put, if the cost of the Expenditures is greater than the Income, the Miners are running at a loss & not making a profit.]
+    ### :red[Expenditures Items]
+    - 🔌 Electricity Cost - the cost of running Bitcoin mining and associated hardware. *
+    - 🖧 Hardware Purchases & Failures - ex. Bitcoin Mining Equipment, Air Conditioners, Wiring etc.
+    - 💸 Rent, taxes, licenses, salaries and other wages for the running of the Bitcoin mining operation.
 
-"""
+    ---
+    - *In this presentation we will only consider "🔌 Electricity Cost" from the :red[Expenditures Items], as it is the majority costs of Bitcoin Mining operations. 
+    
+    - We will however consider all :green[Income Items] in the comparisons.
+
+
+    ### :red[Simply put, if the cost of the Expenditures is greater than the Income, the Miners are running at a loss & not making a profit.]
+
+    """
